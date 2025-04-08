a57815d39d0ed6af0f019e5193c3dd490792f2ef
VicTech99 <victorynnado1@gmail.com>
2025-04-08 19:41:34 +0100

    uploading of Assignment
    Focuses more on JavaScript Data types like Strings, Numbers, Booleans, Null and undefined.

---
+++ index.js
@@ -0,0 +1,65 @@
+
+const firstName = "Nnado";
+
+const middleName = "Somtochukwu";
+
+const lastName = "Victory";
+
+const age = 25;
+
+const myCountry = "Nigeria";
+
+const message = `My name is ${firstName} ${lastName} ${middleName}. I am ${age} years old and i am from ${myCountry}.`
+
+console.log(message);
+
+let favQuote = "There is no Competition in Destiny"
+
+console.log(favQuote.toLocaleUpperCase());
+
+let favLyrics = "Just have Faith and Believe in God"
+
+console.log(favLyrics.toLocaleLowerCase());
+
+const word = "Email";
+const reversedWord = word.split('').reverse().join('');
+console.log(reversedWord);
+
+const item1 = 200;
+const item2 = 120;
+const item3 = 500;
+
+const totalPrice = item1 + item2 + item3;
+
+console.log(`#${totalPrice}`);
+
+
+const score1 = 55;
+const score2 = 65;
+const score3 = 75;
+const score4 = 85;
+const score5 = 95;
+
+const average = (score1 + score2 + score3 + score4 + score5) / 5;
+console.log(`Your average score is ${average}.`);
+
+const favoriteFoods = ["Jollof Rice", "Fried Plantain", "Egusi Soup", "Pounded Yam", "Chicken"];
+
+console.log("First Food:", favoriteFoods[0]);
+console.log("Last Food:", favoriteFoods[favoriteFoods.length - 1]);
+
+favoriteFoods.unshift("Moi Moi"); 
+favoriteFoods.push("Pepper Soup"); 
+
+console.log("Updated Food List:", favoriteFoods);
+
+
+const jssOne = ["Victory", "Victor", "Somtochukwu", "Nick", "Praise", "ben", "Precious", "Divine", "Miracle", "Meme"]
+console.log(jssOne)
+
+const jssTwo = ["Conso", "Kelechi", "Blessing", "Vero", "Chidera", "Lovety", "Chinaza", "Chinonso", "Chiamaka", "Chindo"]
+console.log(jssTwo)
+
+const jssThree = ["Elijah", "Elisha", "Zeru", "Abigail", "Chisom", "Caleb", "Prosper", "Peter", "Canaan", "Chidalu"]
+console.log(jssThree)
+
